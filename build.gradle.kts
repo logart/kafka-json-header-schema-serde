e25b875ecfd71995fc700b8a9edb42b0ba017f92
artem.loginov <artem.loginov@audienzz.ch>
2025-08-12 10:47:42 +0200

    Migrate to gradle. Fix package name. Add action to publish jar on github.

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("java")
+}
+
+group = "com.github.logart"
+version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_19
+    targetCompatibility = JavaVersion.VERSION_19
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://packages.confluent.io/maven/")
+    }
+}
+
+val kafkaVersion = "3.8.0"
+val confluentVersion = "7.9.1"
+
+dependencies {
+    compileOnly("org.apache.kafka:connect-api:${kafkaVersion}")  // provided scope in Maven
+
+    implementation("io.confluent:kafka-json-schema-serializer:${confluentVersion}")
+    implementation("io.confluent:kafka-connect-json-schema-converter:${confluentVersion}")
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+}
